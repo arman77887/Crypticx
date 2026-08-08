c15b55c7cccd67dcfd5ddadd6ea63f7369484831
Mohammad Arman <233636922+arman77887@users.noreply.github.com>
2026-08-08 03:32:33 +0300

    Create page.tsx

---
+++ frontend/app/hosting/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HostingPlans } from "@/components/sections/HostingPlans";
+
+export default function HostingPage() {
+  return (
+    <div className="py-12">
+      <HostingPlans />
+    </div>
+  );
+}

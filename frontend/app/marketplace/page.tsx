c27c6ee55943f3f581deb4cb619386afc995c7d7
Mohammad Arman <233636922+arman77887@users.noreply.github.com>
2026-08-08 03:31:51 +0300

    Create page.tsx

---
+++ frontend/app/marketplace/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MarketplaceGrid } from "@/components/sections/MarketplaceGrid";
+
+export default function MarketplacePage() {
+  return (
+    <div className="py-12">
+      <MarketplaceGrid />
+    </div>
+  );
+}

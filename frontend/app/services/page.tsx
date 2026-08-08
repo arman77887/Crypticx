8a44e663809d95d5fa5350614f20d0f599618dcb
Mohammad Arman <233636922+arman77887@users.noreply.github.com>
2026-08-08 03:31:31 +0300

    Add ServicesPage component with ServicesGrid

---
+++ frontend/app/services/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ServicesGrid } from "@/components/sections/ServicesGrid";
+
+export default function ServicesPage() {
+  return (
+    <div className="py-12">
+      <ServicesGrid />
+    </div>
+  );
+}

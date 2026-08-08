a4e4acb0e6ef6f2c6277ea221b0a697414216259
Mohammad Arman <233636922+arman77887@users.noreply.github.com>
2026-08-08 03:32:10 +0300

    Create CybersecurityPage component with CybersecurityGrid

---
+++ frontend/app/cybersecurity/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CybersecurityGrid } from "@/components/sections/CybersecurityGrid";
+
+export default function CybersecurityPage() {
+  return (
+    <div className="py-12">
+      <CybersecurityGrid />
+    </div>
+  );
+}

4afebeb5767bb60ae2b0abd854121af2bf3d5b65
Mohammad Arman <233636922+arman77887@users.noreply.github.com>
2026-08-08 03:32:52 +0300

    Create page.tsx

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FounderSection } from "@/components/sections/FounderSection";
+
+export default function AboutPage() {
+  return (
+    <div className="py-12">
+      <FounderSection />
+    </div>
+  );
+}
